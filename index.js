bdf33fdf5cd1cbb34da09902dda3eb93ec774b85
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-09-27 18:51:17 +0300

    step3

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ app.get('/', (req, res) => {
     res.send('Ok!')
 })
 
+
+//routes step3
+app.get('/test', (req, res) => {
+    res.status(200).json({ status: 200, message: "ok" })
+})
+
+app.get('/time', (req, res) => {
+    let currentTime = new Date();
+    let hours = currentTime.getHours()
+    let minutes = currentTime.getMinutes()
+    res.status(200).json({ status: 200, message: `${hours}:${minutes}` })
+})
+
 app.listen(3000, () => console.log('this server is listening to 3000 port'));
