3f951e43636b5556ae12cb9cc044b94577326e92
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-09-27 18:53:02 +0300

    step4

--- index.js
+++ index.js
@@ -18,4 +18,17 @@ app.get('/time', (req, res) => {
     res.status(200).json({ status: 200, message: `${hours}:${minutes}` })
 })
 
+
+//route to hello page
+app.get('/hello/:id?', (req, res) => {
+    res.status(200).json({ status: 200, message: (req.params.id) ? `Hello,${req.params.id}` : `Hello!` })
+})
+
+
+//route for search
+app.get('/search', (req, res) => {
+    (req.query.s != undefined)
+        ? res.status(200).json({ status: 200, message: "ok", data: req.query.s })
+        : res.status(500).json({ status: 500, error: true, message: "you have to provide a search" })
+})
 app.listen(3000, () => console.log('this server is listening to 3000 port'));
