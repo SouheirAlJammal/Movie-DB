27712f330a8384fcd8b19deee1d16e4aa3dc5c85
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-09-27 19:00:26 +0300

    step5

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 app = express();
 
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
+
 app.get('/', (req, res) => {
     res.send('Ok!')
 })
@@ -31,4 +39,30 @@ app.get('/search', (req, res) => {
         ? res.status(200).json({ status: 200, message: "ok", data: req.query.s })
         : res.status(500).json({ status: 500, error: true, message: "you have to provide a search" })
 })
+
+
+
+//route to add movie
+app.get('/movies/create',(req,res)=>{
+    res.status(200).send('Done')
+})
+
+
+//route to get movie
+app.get('/movies/read',(req,res)=>{
+    res.status(200).json({status:200, data: movies})
+})
+
+
+//route to update movie
+app.get('/movies/update',(req,res)=>{
+    res.status(200).send('Done')
+})
+
+
+//route to delete movie
+app.get('/movies/delete',(req,res)=>{
+    res.status(200).send('Done')
+})
+
 app.listen(3000, () => console.log('this server is listening to 3000 port'));
